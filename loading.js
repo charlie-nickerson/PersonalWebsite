da857915f381d37ae8d0b4c303f8df4f8bb7a34a
charlie-nickerson <charlie@nickerson.com>
2023-08-09 22:09:38 -0700

    Added a cool background and styled the website using bootstrap

--- loading.js
+++ loading.js
@@ -4,3 +4,14 @@ window.addEventListener("load", function () {
     const mainContent = document.querySelector(".main-content");
     mainContent.style.display = "block"; // Show the main content after loading
   });
+
+
+window.addEventListener("scroll", function () {
+    let scrolled = window.scrollY;
+    let parallaxSections = document.querySelectorAll(".parallax-section");
+  
+    parallaxSections.forEach(function (section) {
+      let speed = section.dataset.speed; // Get the speed value from the data-speed attribute
+      section.style.transform = `translateY(${scrolled * speed}px)`;
+    });
+});
